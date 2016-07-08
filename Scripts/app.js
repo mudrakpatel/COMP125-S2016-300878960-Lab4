18bb9a875db014293af81c348476746ba1ccff72
Mudrak <mudrakpatel96@gmail.com>
2016-07-08 13:14:42 -0500

    Using querySelector method

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,30 +11,29 @@
  * This is an self executing function
  * A.K.A IFEE
  */
-(function() {
+(function () {
     /**
      * Hooking needed elements on the webpage using javascript
      */
-    var h1=document.getElementById("h1");
-    h1.innerHTML="Console Checking Form";
-    var legend=document.getElementById("legend");
-    legend.innerHTML="300878960";
-    var form=document.getElementById("form");
-    var username=document.getElementById("username").nodeValue;
-    var password=document.getElementById("password").nodeValue;
-    var submitButton=document.getElementById("submitButton");
-    submitButton.innerHTML="Login";	
-    /**
-     * Adding event listener to button element present on the webpage
-     */
-    submitButton.addEventListener("click",buttonIsClicked);
-    function buttonIsClicked() {
+    var h1 = document.getElementById("h1");
+    h1.innerHTML = "Console Checking Form";
+    var legend = document.getElementById("legend");
+    legend.innerHTML = "300878960";
+    var form = document.getElementById("form");
+    var username = document.getElementById("username").nodeValue;
+    var password = document.getElementById("password").nodeValue;
+    var submitButton = document.getElementById("submitButton");
+    submitButton.innerHTML = "Login";
+    
+    document.querySelector('form.form').addEventListener('submit', function (e) {
+
+        //prevent the normal submission of the form
+        e.preventDefault();
         console.log("App Started");
-        
         var UserObject = new Object();
-        UserObject.userName=username;
-        UserObject.Password=password;
+        UserObject.userName = username;
+        UserObject.Password = password;
         console.log(UserObject.userName);
         console.log(UserObject.Password);
-    }
+    });
 })();
